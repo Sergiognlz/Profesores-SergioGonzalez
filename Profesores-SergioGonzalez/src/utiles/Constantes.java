7b7c304c2b27f603609d3a5c9292968d74a606f5
sgonzalez <sgonzalez@106-03.info2>
2025-05-19 12:20:15 +0200

    Empezando proyecto

---
+++ Profesores-SergioGonzalez/src/utiles/Constantes.java
@@ -0,0 +1,9 @@
+package utiles;
+
+public class Constantes {
+	public final static String URL = "jdbc:mysql://localhost/institutodb";
+	public final static String USUARIO = "root";
+	public final static String CONTRASEÑA = "Nuevacuenta13*";
+	
+	
+}
